e257dc203550a8dfe7a40b7ae7edc22c98a67416
den0ver <dlalernen@gmail.com>
2026-05-17 17:16:37 +0200

    Add templates, views and update base.css

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def product_list(request, category_slug=None):
-    pass
+    category = None
+    product_list = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        product_list = product_list.filter(category=category)
+    categories = Category.objects.all()
+    paginator = Paginator(product_list, 20)
+    page_number = request.GET.get('page', 1)
+    try:
+        products = paginator.page(page_number)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    context = {'category': category, 'categories': categories, 'products': products}
+    return render(request, 'shop/list.html', context)
 
 
 def product_detail(request, id, slug):
-    pass
+    product = get_object_or_404(Product, id=id, slug=slug)
+    context = {'product': product}
+    return render(request, 'shop/detail.html', context)
